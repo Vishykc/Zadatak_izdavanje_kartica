95466954d32ed9092c3695efde4b5646c6025120
Viktor Vranar <viktor.vranar@gmail.com>
2023-05-05 21:44:40 +0200

    dodan inicijalni spring initializr projekt i upute za zadatak

---
+++ Spring_Boot_Zadatak_izdavanje_kartica/src/main/java/com/github/Spring_Boot_Zadatak_izdavanje_kartica/SpringBootZadatakIzdavanjeKarticaApplication.java
@@ -0,0 +1,13 @@
+package com.github.Spring_Boot_Zadatak_izdavanje_kartica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZadatakIzdavanjeKarticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootZadatakIzdavanjeKarticaApplication.class, args);
+	}
+
+}
---
+++ Spring_Boot_Zadatak_izdavanje_kartica/src/test/java/com/github/Spring_Boot_Zadatak_izdavanje_kartica/SpringBootZadatakIzdavanjeKarticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Spring_Boot_Zadatak_izdavanje_kartica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZadatakIzdavanjeKarticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
